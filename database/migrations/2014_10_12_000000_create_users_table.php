9507640d75c7eabb1200d7efdecffbc3d0da2a85
rdirhamcatur <rdirhamcatur@gmail.com>
2023-12-08 14:29:18 +0800

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('tbl_pegawai', function (Blueprint $table) {
             $table->id();
             $table->string('pegawai_nama');
-            $table->tinyInt('pegawai_umur');
-            $table->longText('pegawai_alamat');
+            $table->string('pegawai_umur');
+            $table->string('pegawai_alamat');
             $table->string('foto')->nullable();;
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
